42c1211d5a04f1456ed278bebc0e9ef94fe5ae18
mohamedis8890 <mohamed.is8890@gmail.com>
2023-04-17 04:06:43 +0200

    Correct Image URL

--- azure/src/functions/getImages.js
+++ azure/src/functions/getImages.js
@@ -30,7 +30,7 @@ app.http("getImages", {
 
     // List all blobs in the container
     for await (const blob of containerClient.listBlobsFlat()) {
-      const imageUrl = `${blob.name}?${sasToken}`;
+      const imageUrl = `${blob.name}`;
       const url = `https://${accountName}.blob.core.windows.net/images/${imageUrl}`;
       imageUrls.push({ url, name: blob.name });
     }
